cd89a8df7e8e083f590e4bbd45e17cdeb2bbff6d
mosleh <moslehazizi@gmail.com>
2023-09-26 21:58:21 +0330

    CustomUser model was created!

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,52 +1,30 @@
-from django.urls import reverse
-from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.test import TestCase
 
-class CustomUserTest(TestCase):
+class CustomUserTests(TestCase):
 
     def test_create_user(self):
-        user = get_user_model().objects.create_user(
-            username = 'testuser',
-            email = 'testuser@email.com',
-            password = 'testpass123',
+        User = get_user_model()
+        user = User.objects.create_user(
+        username='will',
+        email='will@email.com',
+        password='testpass123'
         )
-        self.assertEqual(user.username, 'testuser')
-        self.assertEqual(user.email, 'testuser@email.com')
+        self.assertEqual(user.username, 'will')
+        self.assertEqual(user.email, 'will@email.com')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
 
     def test_create_superuser(self):
-        user = get_user_model().objects.create_superuser(
-            username = 'testsuperuser',
-            email = 'testsuperuser@email.com',
-            password = 'testpass123',
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+        username='superadmin',
+        email='superadmin@email.com',
+        password='testpass123'
         )
-        self.assertEqual(user.username, 'testsuperuser')
-        self.assertEqual(user.email, 'testsuperuser@email.com')
-        self.assertTrue(user.is_active)
-        self.assertTrue(user.is_staff)
-        self.assertTrue(user.is_superuser)
-
-class SignupTest(TestCase):
-
-    username = 'newuser'
-    email = 'newuser@email.com'
-
-    def setUp(self):
-        url = reverse('account_signup')
-        self.response = self.client.get(url)
-
-    def test_signup_template(self):
-        self.assertEqual(self.response.status_code, 200)
-        self.assertTemplateUsed(self.response, 'account/signup.html')
-        self.assertContains(self.response, 'Sign Up')
-        self.assertNotContains(self.response, 'Hey! I should not be on this page.')
-
-    def test_signup_form(self):
-        new_user = get_user_model().objects.create_user(
-            self.username, self.email
-        )
-        self.assertEqual(get_user_model().objects.all().count(), 1)
-        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
-        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
--- points/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-        path('', include('pages.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('courses/', include('classManagement.urls')),
-]
