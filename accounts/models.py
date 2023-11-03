f7fe7ec3cdbf446c39cfd8e4a626ffc668ebb34f
mosleh <moslehazizi@gmail.com>
2023-11-03 22:18:51 +0330

    Update templates

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     
     def __str__(self):
-        return f'{self.username}'
+        return f'{self.first_name} {self.last_name}'
