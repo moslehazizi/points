0fa5c7e8fa21884291ab71dda718e886e28171cf
mosleh <moslehazizi@gmail.com>
2023-11-03 19:45:10 +0330

    update scripts

--- scripts/user_load.py
+++ scripts/user_load.py
@@ -1,6 +1,7 @@
 import csv
 from accounts.models import CustomUser
 from pages.models import Person
+from django.contrib.auth.hashers import make_password
 
 def run():
     fhand = open('csvFiles/UserInfo_1.csv')
@@ -12,12 +13,14 @@ def run():
 
     for row in reader:
         print(row)
-
-        u, created = CustomUser.objects.get_or_create(username=row[0], email=row[1], password=row[2], first_name=row[3], 
-                        last_name=row[4], )
+        
+        u, created = CustomUser.objects.get_or_create(username=row[0], email=row[1],
+                                                       first_name=row[3], last_name=row[4])
+        CustomUser.password = CustomUser.set_password(u, raw_password=str(row[2]))      
         r = Person.STUDENT
         if row[5] == 'T' : r = Person.TEACHER
 
-        p = Person(person=u, father_name=row[7], phone_number=row[8], role=r)
-
+        
+        p = Person(person=u, father_name=row[6], phone_number=row[7], role=r)
         p.save()
+        u.save()
